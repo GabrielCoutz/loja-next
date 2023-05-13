86857fe4571008f34afaf99b2611c227c5c889c6
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-13 17:53:26 -0300

    feat(api/product): created methods to product endpoint

---
+++ src/services/api/product/index.ts
@@ -0,0 +1,25 @@
+import { IProduct, IProductList } from "./interface";
+
+const customFetch = (url: string) => {
+  return fetch(url, {
+    next: {
+      revalidate: 30,
+    },
+  });
+};
+
+export const getProducts = async (): Promise<IProductList> => {
+  const { products }: Record<"products", IProductList> = await (
+    await customFetch(`${process.env.API_URL}/products`)
+  ).json();
+
+  return products;
+};
+
+export const getSingleProduct = async (id: string): Promise<IProduct> => {
+  const product: IProduct = await (
+    await customFetch(`${process.env.API_URL}/products/${id}`)
+  ).json();
+
+  return product;
+};
