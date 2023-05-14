69a8bf5bc1f8e79039e1ea3a88bd3c45d6188128
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-14 15:28:52 -0300

    refactor(productmodal): changed name of env var

--- src/app/product/@modal/[id]/show/[index]/page.tsx
+++ src/app/product/@modal/[id]/show/[index]/page.tsx
@@ -12,7 +12,7 @@ interface ModalProps {
 
 const Modal = async ({ params }: ModalProps) => {
   const product: IProduct = await (
-    await fetch(`${process.env.API_URL}/products/${params.id}`)
+    await fetch(`${process.env.PRODUCTS_API_URL}/products/${params.id}`)
   ).json();
 
   return (
