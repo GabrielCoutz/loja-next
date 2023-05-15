47be2f17ff2f882fcfad58ec505e3fcd688beeec
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-15 12:48:10 -0300

    feat(form/input): add error style

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -9,12 +9,19 @@ interface InputProps extends HtmlHTMLAttributes<HTMLInputElement> {
 }
 
 export const Input = (props: InputProps) => {
-  const { register } = useFormContext();
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const hasErro = !!errors[props.name]?.message?.toString();
 
   return (
     <input
       {...register(props.name)}
-      className="bg-zinc-200 rounded"
+      id={props.name}
+      className={`bg-zinc-200 rounded p-2 mt-1 transition outline-none focus:ring-2 focus:ring-indigo-500 active:ring-2 active:ring-indigo-500 ${
+        hasErro ? "ring-2 ring-inset !ring-red-500" : ""
+      }`}
       {...props}
     />
   );
